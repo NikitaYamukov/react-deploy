019ebcdc46b83b7363dbc91e4aa2aadc1e20e46b
Nikita Yamukov <nikita.yamukov@gmail.com>
2020-02-14 03:54:13 +0300

    Updates

---
+++ precache-manifest.e54177aa2f9b7a34a1b3409d77c06c1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f56ce79be594cffca7729bef39fbb59",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "75ea751ee3f26c2b0646",
+    "url": "/react-deploy/static/css/main.9afd199a.chunk.css"
+  },
+  {
+    "revision": "0544f7ac6b24d726a50a",
+    "url": "/react-deploy/static/js/2.7459da1a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-deploy/static/js/2.7459da1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ea751ee3f26c2b0646",
+    "url": "/react-deploy/static/js/main.535c80a2.chunk.js"
+  },
+  {
+    "revision": "b2f342cd0627d3529f0e",
+    "url": "/react-deploy/static/js/runtime-main.dc251424.js"
+  }
+]);
